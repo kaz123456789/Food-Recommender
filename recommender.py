93e527cf3fb1da2932fd84dc256e356cf5dfeed0
Kathleen <115058370+kaz123456789@users.noreply.github.com>
2024-03-16 16:55:48 -0400

    Update recommender.py

---
+++ recommender.py
@@ -0,0 +1,102 @@
+"""
+CSC111 Project 2: Food Recommender
+
+Module Description
+==================
+This module contains the Tree class and various methods and functions.
+
+Copyright and Usage Information
+===============================
+
+his file is Copyright (c) Kathleen Wang, Jiner Zhang, Kimberly Fu, and Yanting Fan.
+"""
+import csv
+from typing import Any, Optional
+
+from python_ta.contracts import check_contracts
+
+
+# @check_contracts - We are commenting this out, so it doesn't slow down the code for Part 2.2
+class Tree:
+    """A recursive tree data structure.
+
+    Note the relationship between this class and RecursiveList; the only major
+    difference is that _rest has been replaced by _subtrees to handle multiple
+    recursive sub-parts.
+
+    Representation Invariants:
+        - self._root is not None or self._subtrees == []
+        - all(not subtree.is_empty() for subtree in self._subtrees)
+    """
+    # Private Instance Attributes:
+    #   - _root:
+    #       The item stored at this tree's root, or None if the tree is empty.
+    #   - _subtrees:
+    #       The list of subtrees of this tree. This attribute is empty when
+    #       self._root is None (representing an empty tree). However, this attribute
+    #       may be empty when self._root is not None, which represents a tree consisting
+    #       of just one item.
+    _root: Optional[Any]
+    _subtrees: list[Tree]
+
+    def __init__(self, root: Optional[Any], subtrees: list[Tree]) -> None:
+        """Initialize a new Tree with the given root value and subtrees.
+
+        If root is None, the tree is empty.
+
+        Preconditions:
+            - root is not none or subtrees == []
+        """
+        self._root = root
+        self._subtrees = subtrees
+
+    def is_empty(self) -> bool:
+        """Return whether this tree is empty.
+
+        >>> t1 = Tree(None, [])
+        >>> t1.is_empty()
+        True
+        >>> t2 = Tree(3, [])
+        >>> t2.is_empty()
+        False
+        """
+        return self._root is None
+
+    def __len__(self) -> int:
+        """Return the number of items contained in this tree.
+
+        >>> t1 = Tree(None, [])
+        >>> len(t1)
+        0
+        >>> t2 = Tree(3, [Tree(4, []), Tree(1, [])])
+        >>> len(t2)
+        3
+        """
+        if self.is_empty():
+            return 0
+        else:
+            size = 1  # count the root
+            for subtree in self._subtrees:
+                size += subtree.__len__()  # could also write len(subtree)
+            return size
+
+    def __contains__(self, item: Any) -> bool:
+        """Return whether the given is in this tree.
+
+        >>> t = Tree(1, [Tree(2, []), Tree(5, [])])
+        >>> t.__contains__(1)
+        True
+        >>> t.__contains__(5)
+        True
+        >>> t.__contains__(4)
+        False
+        """
+        if self.is_empty():
+            return False
+        elif self._root == item:
+            return True
+        else:
+            for subtree in self._subtrees:
+                if subtree.__contains__(item):
+                    return True
+            return False
