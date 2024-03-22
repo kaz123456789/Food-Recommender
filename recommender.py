01bc57a3abd96f3ea9aeafc13385084e76a90fd1
fries anyone <ffanyanting@gmail.com>
2024-03-22 16:27:39 -0400

    hi
    hi

--- recommender.py
+++ recommender.py
@@ -16,90 +16,145 @@ from typing import Any, Optional
 from python_ta.contracts import check_contracts
 
 
-@check_contracts
-class Tree:
-    """A recursive tree data structure.
+class _Vertex:
+    """A vertex in a book review graph, used to represent a user or a book.
 
-    Note the relationship between this class and RecursiveList; the only major
-    difference is that _rest has been replaced by _subtrees to handle multiple
-    recursive sub-parts.
+    Each vertex item is either a user id or book title. Both are represented as strings,
+    even though we've kept the type annotation as Any to be consistent with lecture.
+
+    Instance Attributes:
+        - item: The data stored in this vertex, representing a user or book.
+        - kind: The type of this vertex: 'user' or 'book'.
+        - neighbours: The vertices that are adjacent to this vertex.
 
     Representation Invariants:
-        - self._root is not None or self._subtrees == []
-        - all(not subtree.is_empty() for subtree in self._subtrees)
+        - self not in self.neighbours
+        - all(self in u.neighbours for u in self.neighbours)
+        - self.kind in {'user', 'book'}
     """
-    # Private Instance Attributes:
-    #   - _root:
-    #       The item stored at this tree's root, or None if the tree is empty.
-    #   - _subtrees:
-    #       The list of subtrees of this tree. This attribute is empty when
-    #       self._root is None (representing an empty tree). However, this attribute
-    #       may be empty when self._root is not None, which represents a tree consisting
-    #       of just one item.
-    _root: Optional[Any]
-    _subtrees: list[Tree]
+    item: Any
+    kind: str
+    neighbours: set[_Vertex]
 
-    def __init__(self, root: Optional[Any], subtrees: list[Tree]) -> None:
-        """Initialize a new Tree with the given root value and subtrees.
+    def __init__(self, item: Any, kind: str) -> None:
+        """Initialize a new vertex with the given item and kind.
 
-        If root is None, the tree is empty.
+        This vertex is initialized with no neighbours.
 
         Preconditions:
-            - root is not none or subtrees == []
+            - kind in {'user', 'book'}
         """
-        self._root = root
-        self._subtrees = subtrees
+        self.item = item
+        self.kind = kind
+        self.neighbours = set()
 
-    def is_empty(self) -> bool:
-        """Return whether this tree is empty.
+    def degree(self) -> int:
+        """Return the degree of this vertex."""
+        return len(self.neighbours)
 
-        >>> t1 = Tree(None, [])
-        >>> t1.is_empty()
-        True
-        >>> t2 = Tree(3, [])
-        >>> t2.is_empty()
-        False
+
+class Graph:
+    """A graph used to represent a book review network.
+    """
+    # Private Instance Attributes:
+    #     - _vertices:
+    #         A collection of the vertices contained in this graph.
+    #         Maps item to _Vertex object.
+    _vertices: dict[Any, _Vertex]
+
+    def __init__(self) -> None:
+        """Initialize an empty graph (no vertices or edges)."""
+        self._vertices = {}
+
+    def add_vertex(self, item: Any, kind: str) -> None:
+        """Add a vertex with the given item and kind to this graph.
+
+        The new vertex is not adjacent to any other vertices.
+        Do nothing if the given item is already in this graph.
+
+        Preconditions:
+            - kind in {'user', 'book'}
         """
-        return self._root is None
+        if item not in self._vertices:
+            self._vertices[item] = _Vertex(item, kind)
 
-    def __len__(self) -> int:
-        """Return the number of items contained in this tree.
+    def add_edge(self, item1: Any, item2: Any) -> None:
+        """Add an edge between the two vertices with the given items in this graph.
 
-        >>> t1 = Tree(None, [])
-        >>> len(t1)
-        0
-        >>> t2 = Tree(3, [Tree(4, []), Tree(1, [])])
-        >>> len(t2)
-        3
+        Raise a ValueError if item1 or item2 do not appear as vertices in this graph.
+
+        Preconditions:
+            - item1 != item2
         """
-        if self.is_empty():
-            return 0
+        if item1 in self._vertices and item2 in self._vertices:
+            v1 = self._vertices[item1]
+            v2 = self._vertices[item2]
+
+            v1.neighbours.add(v2)
+            v2.neighbours.add(v1)
         else:
-            size = 1  # count the root
-            for subtree in self._subtrees:
-                size += subtree.__len__()  # could also write len(subtree)
-            return size
+            raise ValueError
 
-    def __contains__(self, item: Any) -> bool:
-        """Return whether the given is in this tree.
+    def adjacent(self, item1: Any, item2: Any) -> bool:
+        """Return whether item1 and item2 are adjacent vertices in this graph.
 
-        >>> t = Tree(1, [Tree(2, []), Tree(5, [])])
-        >>> t.__contains__(1)
-        True
-        >>> t.__contains__(5)
-        True
-        >>> t.__contains__(4)
-        False
+        Return False if item1 or item2 do not appear as vertices in this graph.
         """
-        if self.is_empty():
-            return False
-        elif self._root == item:
-            return True
+        if item1 in self._vertices and item2 in self._vertices:
+            v1 = self._vertices[item1]
+            return any(v2.item == item2 for v2 in v1.neighbours)
         else:
-            for subtree in self._subtrees:
-                if subtree.__contains__(item):
-                    return True
             return False
+
+    def get_neighbours(self, item: Any) -> set:
+        """Return a set of the neighbours of the given item.
+
+        Note that the *items* are returned, not the _Vertex objects themselves.
+
+        Raise a ValueError if item does not appear as a vertex in this graph.
+        """
+        if item in self._vertices:
+            v = self._vertices[item]
+            return {neighbour.item for neighbour in v.neighbours}
+        else:
+            raise ValueError
+
+    def get_all_vertices(self, kind: str = '') -> set:
+        """Return a set of all vertex items in this graph.
+
+        If kind != '', only return the items of the given vertex kind.
+
+        Preconditions:
+            - kind in {'', 'user', 'book'}
+        """
+        if kind != '':
+            return {v.item for v in self._vertices.values() if v.kind == kind}
+        else:
+            return set(self._vertices.keys())
+
+    def to_networkx(self, max_vertices: int = 5000) -> nx.Graph:
+        """Convert this graph into a networkx Graph.
+
+        max_vertices specifies the maximum number of vertices that can appear in the graph.
+        (This is necessary to limit the visualization output for large graphs.)
+
+        Note that this method is provided for you, and you shouldn't change it.
+        """
+        graph_nx = nx.Graph()
+        for v in self._vertices.values():
+            graph_nx.add_node(v.item, kind=v.kind)
+
+            for u in v.neighbours:
+                if graph_nx.number_of_nodes() < max_vertices:
+                    graph_nx.add_node(u.item, kind=u.kind)
+
+                if u.item in graph_nx.nodes:
+                    graph_nx.add_edge(v.item, u.item)
+
+            if graph_nx.number_of_nodes() >= max_vertices:
+                break
+
+        return graph_nx
 
 
 if __name__ == '__main__':
