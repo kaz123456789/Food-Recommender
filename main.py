267f5a8be23bf5c6ff29e7abf273b31ccb5d03f3
jinerfreya <149322974+jinerfreya@users.noreply.github.com>
2024-03-25 21:29:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 
 from __future__ import annotations
 import csv
-from typing import Any, Union
+from typing import Any
 
 import networkx as nx
 
 
 class _Vertex:
     """A vertex in a restaurant graph, used to represent a restaurant.
-
     Each vertex item is either a restaurant name..
-
     Instance Attributes:
         - item: The data stored in this vertex, representing a user or book.
         - kind: The type of this vertex: ''.
         - neighbours: The vertices that are adjacent to this vertex.
-
     Representation Invariants:
         - self not in self.neighbours
         - all(self in u.neighbours for u in self.neighbours)
@@ -25,19 +22,21 @@ class _Vertex:
     price: int
     latitude: float
     longitude: float
+    review_rate: float
     neighbours: set[_Vertex]
 
-    def __init__(self, name: str, kind: str, price: int, latitude: float, longitude: float) -> None:
+    def __init__(self, category: str, address: str, name: str, price_range: str, latitude: float, longitude: float,
+                 review_rate: float) -> None:
         """Initialize a new vertex with the given item and kind.
-
         This vertex is initialized with no neighbours.
-
         """
+        self.category = category
+        self.address = address
         self.name = name
-        self.kind = kind
-        self.price = price
+        self.price_range = price_range
         self.latitude = latitude
         self.longitude = longitude
+        self.review_rate = review_rate
         self.neighbours = set()
 
     def degree(self) -> int:
@@ -58,21 +57,18 @@ class Graph:
         """Initialize an empty graph (no vertices or edges)."""
         self._vertices = {}
 
-    def add_vertex(self, name: str, kind: str, price: int, latitude: float, longitude: float) -> None:
+    def add_vertex(self, category: str, address: str, name: str, price_range: str, latitude: float, longitude: float,
+                   review_rate: float) -> None:
         """Add a vertex with the given restaurant details to this graph.
-
         The new vertex is not adjacent to any other vertices.
         Do nothing if the given restaurant is already in this graph.
-
         """
         if name not in self._vertices:
-            self._vertices[name] = _Vertex(name, kind, price, latitude, longitude)
+            self._vertices[name] = _Vertex(category, address, name, price_range, latitude, longitude, review_rate)
 
     def add_edge(self, name1: Any, name2: Any) -> None:
         """Add an edge between the two vertices with the given restaurant names in this graph.
-
         Raise a ValueError if name1 or name2 do not appear as vertices in this graph.
-
         Preconditions:
             - name1 != name2
         """
@@ -87,7 +83,6 @@ class Graph:
 
     def adjacent(self, name1: Any, name2: Any) -> bool:
         """Return whether name1 and name2 are adjacent vertices in this graph.
-
         Return False if name1 or name2 do not appear as vertices in this graph.
         """
         if name1 in self._vertices and name2 in self._vertices:
@@ -98,9 +93,7 @@ class Graph:
 
     def get_neighbours(self, name: Any) -> set:
         """Return a set of the neighbours of the given name.
-
         Note that the *names* are returned, not the _Vertex objects themselves.
-
         Raise a ValueError if name does not appear as a vertex in this graph.
         """
         if name in self._vertices:
@@ -111,9 +104,7 @@ class Graph:
 
     def get_all_vertices(self, kind: str = '') -> set:
         """Return a set of all vertex names in this graph.
-
         If kind != '', only return the items of the given vertex kind.
-
         """
         if kind != '':
             return {v.name for v in self._vertices.values() if v.kind == kind}
@@ -122,10 +113,8 @@ class Graph:
 
     def to_networkx(self, max_vertices: int = 5000) -> nx.Graph:
         """Convert this graph into a networkx Graph.
-
         max_vertices specifies the maximum number of vertices that can appear in the graph.
         (This is necessary to limit the visualization output for large graphs.)
-
         Note that this method is provided for you, and you shouldn't change it.
         """
         graph_nx = nx.Graph()
@@ -144,3 +133,25 @@ class Graph:
 
         return graph_nx
 
+    def load_graph(self, rest_file: str) -> Graph:
+        """Return a restaurant graph corresponding to the given datasets.
+
+        The CSV file should have the columns 'Category', 'Restaurant Address', 'Name',
+        'Restaurant Price Range', 'Restaurant Latitude', 'Restaurant Longitude'
+        and 'Review Rates'.
+        """
+        graph = Graph()
+
+        with open(rest_file, 'r') as file:
+            next(file)
+            reader = csv.reader(file)
+            for row in reader:
+                category, address, name, price_range, latitude, longitude, review_rate = row
+                price_min, price_max = map(int, price_range.replace('$', '').split('-'))
+                latitude = float(latitude)
+                longitude = float(longitude)
+                review_rate = float(review_rate)
+
+                graph.add_vertex(category, address, name, price_range, latitude, longitude, review_rate)
+
+        return graph
