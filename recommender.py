58650f7f313db507c8e17ef635cbb1b0195b7ac9
fries anyone <157916054+faenwitee@users.noreply.github.com>
2024-03-27 15:42:16 -0400

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -10,42 +10,43 @@ This module contains the Tree class and various methods and functions.
 his file is Copyright (c) Kathleen Wang, Jiner Zhang, Kimberly Fu, and Yanting Fan.
 """
 from __future__ import annotations
-
 import csv
-from typing import Any, Optional
-from python_ta.contracts import check_contracts
+from typing import Any
+
+import networkx as nx
 
 
 class _Vertex:
-    """A vertex in a book review graph, used to represent a user or a book.
-
-    Each vertex item is either a user id or book title. Both are represented as strings,
-    even though we've kept the type annotation as Any to be consistent with lecture.
-
+    """A vertex in a restaurant graph, used to represent a restaurant.
+    Each vertex item is either a restaurant name..
     Instance Attributes:
         - item: The data stored in this vertex, representing a user or book.
-        - kind: The type of this vertex: 'user' or 'book'.
+        - kind: The type of this vertex: ''.
         - neighbours: The vertices that are adjacent to this vertex.
-
     Representation Invariants:
         - self not in self.neighbours
         - all(self in u.neighbours for u in self.neighbours)
-        - self.kind in {'user', 'book'}
     """
-    item: Any
+    name: Any
     kind: str
+    price: int
+    latitude: float
+    longitude: float
+    review_rate: float
     neighbours: set[_Vertex]
 
-    def __init__(self, item: Any, kind: str) -> None:
+    def __init__(self, category: str, address: str, name: str, price_range: str, latitude: float, longitude: float,
+                 review_rate: float) -> None:
         """Initialize a new vertex with the given item and kind.
-
         This vertex is initialized with no neighbours.
-
-        Preconditions:
-            - kind in {'user', 'book'}
         """
-        self.item = item
-        self.kind = kind
+        self.category = category
+        self.address = address
+        self.name = name
+        self.price_range = price_range
+        self.latitude = latitude
+        self.longitude = longitude
+        self.review_rate = review_rate
         self.neighbours = set()
 
     def degree(self) -> int:
@@ -54,7 +55,7 @@ class _Vertex:
 
 
 class Graph:
-    """A graph used to represent a book review network.
+    """A graph used to represent .
     """
     # Private Instance Attributes:
     #     - _vertices:
@@ -66,292 +67,101 @@ class Graph:
         """Initialize an empty graph (no vertices or edges)."""
         self._vertices = {}
 
-    def add_vertex(self, item: Any, kind: str) -> None:
-        """Add a vertex with the given item and kind to this graph.
-
+    def add_vertex(self, category: str, address: str, name: str, price_range: str, latitude: float, longitude: float,
+                   review_rate: float) -> None:
+        """Add a vertex with the given restaurant details to this graph.
         The new vertex is not adjacent to any other vertices.
-        Do nothing if the given item is already in this graph.
-
-        Preconditions:
-            - kind in {'user', 'book'}
+        Do nothing if the given restaurant is already in this graph.
         """
-        if item not in self._vertices:
-            self._vertices[item] = _Vertex(item, kind)
+        if name not in self._vertices:
+            self._vertices[name] = _Vertex(category, address, name, price_range, latitude, longitude, review_rate)
 
-    def add_edge(self, item1: Any, item2: Any) -> None:
-        """Add an edge between the two vertices with the given items in this graph.
-
-        Raise a ValueError if item1 or item2 do not appear as vertices in this graph.
-
+    def add_edge(self, name1: Any, name2: Any) -> None:
+        """Add an edge between the two vertices with the given restaurant names in this graph.
+        Raise a ValueError if name1 or name2 do not appear as vertices in this graph.
         Preconditions:
-            - item1 != item2
+            - name1 != name2
         """
-        if item1 in self._vertices and item2 in self._vertices:
-            v1 = self._vertices[item1]
-            v2 = self._vertices[item2]
+        if name1 in self._vertices and name2 in self._vertices:
+            v1 = self._vertices[name1]
+            v2 = self._vertices[name2]
 
             v1.neighbours.add(v2)
             v2.neighbours.add(v1)
         else:
             raise ValueError
 
-    def adjacent(self, item1: Any, item2: Any) -> bool:
-        """Return whether item1 and item2 are adjacent vertices in this graph.
-
-        Return False if item1 or item2 do not appear as vertices in this graph.
+    def adjacent(self, name1: Any, name2: Any) -> bool:
+        """Return whether name1 and name2 are adjacent vertices in this graph.
+        Return False if name1 or name2 do not appear as vertices in this graph.
         """
-        if item1 in self._vertices and item2 in self._vertices:
-            v1 = self._vertices[item1]
-            return any(v2.item == item2 for v2 in v1.neighbours)
+        if name1 in self._vertices and name2 in self._vertices:
+            v1 = self._vertices[name1]
+            return any(v2.name == name2 for v2 in v1.neighbours)
         else:
             return False
 
-    def get_neighbours(self, item: Any) -> set:
-        """Return a set of the neighbours of the given item.
-
-        Note that the *items* are returned, not the _Vertex objects themselves.
-
-        Raise a ValueError if item does not appear as a vertex in this graph.
+    def get_neighbours(self, name: Any) -> set:
+        """Return a set of the neighbours of the given name.
+        Note that the *names* are returned, not the _Vertex objects themselves.
+        Raise a ValueError if name does not appear as a vertex in this graph.
         """
-        if item in self._vertices:
-            v = self._vertices[item]
-            return {neighbour.item for neighbour in v.neighbours}
+        if name in self._vertices:
+            v = self._vertices[name]
+            return {neighbour.name for neighbour in v.neighbours}
         else:
             raise ValueError
 
     def get_all_vertices(self, kind: str = '') -> set:
-        """Return a set of all vertex items in this graph.
-
+        """Return a set of all vertex names in this graph.
         If kind != '', only return the items of the given vertex kind.
-
-        Preconditions:
-            - kind in {'', 'user', 'book'}
         """
         if kind != '':
-            return {v.item for v in self._vertices.values() if v.kind == kind}
+            return {v.name for v in self._vertices.values() if v.kind == kind}
         else:
             return set(self._vertices.keys())
 
     def to_networkx(self, max_vertices: int = 5000) -> nx.Graph:
         """Convert this graph into a networkx Graph.
-
         max_vertices specifies the maximum number of vertices that can appear in the graph.
         (This is necessary to limit the visualization output for large graphs.)
-
         Note that this method is provided for you, and you shouldn't change it.
         """
         graph_nx = nx.Graph()
         for v in self._vertices.values():
-            graph_nx.add_node(v.item, kind=v.kind)
+            graph_nx.add_node(v.name, kind=v.kind, price=v.price, latitude=v.latitute, longitude=v.longitude)
 
             for u in v.neighbours:
                 if graph_nx.number_of_nodes() < max_vertices:
-                    graph_nx.add_node(u.item, kind=u.kind)
+                    graph_nx.add_node(u.name, kind=u.kind, price=u.price, latitude=u.latitute, longitude=u.longitude)
 
-                if u.item in graph_nx.nodes:
-                    graph_nx.add_edge(v.item, u.item)
-
-            if graph_nx.number_of_nodes() >= max_vertices:
-                break
-
-        return graph_nx
-
-
-class _WeightedVertex(_Vertex):
-    """A vertex in a weighted book review graph, used to represent a user or a book.
-
-    Same documentation as _Vertex from Exercise 3, except now neighbours is a dictionary mapping
-    a neighbour vertex to the weight of the edge to from self to that neighbour.
-    Note that for this exercise, the weights will be integers between 1 and 5.
-
-    Instance Attributes:
-        - item: The data stored in this vertex, representing a user or book.
-        - kind: The type of this vertex: 'user' or 'book'.
-        - neighbours: The vertices that are adjacent to this vertex, and their corresponding
-            edge weights.
-
-    Representation Invariants:
-        - self not in self.neighbours
-        - all(self in u.neighbours for u in self.neighbours)
-        - self.kind in {'user', 'book'}
-    """
-    item: Any
-    kind: str
-    neighbours: dict[_WeightedVertex, Union[int, float]]
-
-    def __init__(self, item: Any, kind: str) -> None:
-        """Initialize a new vertex with the given item and kind.
-
-        This vertex is initialized with no neighbours.
-
-        Preconditions:
-            - kind in {'user', 'book'}
-        """
-        super().__init__(item, kind)
-        self.neighbours = {}
-
-
-class WeightedGraph(Graph):
-    """A weighted graph used to represent a book review network that keeps track of review scores.
-
-    Note that this is a subclass of the Graph class from Exercise 3, and so inherits any methods
-    from that class that aren't overridden here.
-    """
-    # Private Instance Attributes:
-    #     - _vertices:
-    #         A collection of the vertices contained in this graph.
-    #         Maps item to _WeightedVertex object.
-    _vertices: dict[Any, _WeightedVertex]
-
-    def __init__(self) -> None:
-        """Initialize an empty graph (no vertices or edges)."""
-        self._vertices = {}
-
-        # This call isn't necessary, except to satisfy PythonTA.
-        Graph.__init__(self)
-
-    def add_vertex(self, item: Any, kind: str) -> None:
-        """Add a vertex with the given item and kind to this graph.
-
-        The new vertex is not adjacent to any other vertices.
-        Do nothing if the given item is already in this graph.
-
-        Preconditions:
-            - kind in {'user', 'book'}
-        """
-        if item not in self._vertices:
-            self._vertices[item] = _WeightedVertex(item, kind)
-
-    def add_edge(self, item1: Any, item2: Any, weight: Union[int, float] = 1) -> None:
-        """Add an edge between the two vertices with the given items in this graph,
-        with the given weight.
-
-        Raise a ValueError if item1 or item2 do not appear as vertices in this graph.
-
-        Preconditions:
-            - item1 != item2
-        """
-        if item1 in self._vertices and item2 in self._vertices:
-            v1 = self._vertices[item1]
-            v2 = self._vertices[item2]
-
-            # Add the new edge
-            v1.neighbours[v2] = weight
-            v2.neighbours[v1] = weight
-        else:
-            # We didn't find an existing vertex for both items.
-            raise ValueError
-
-    def get_weight(self, item1: Any, item2: Any) -> Union[int, float]:
-        """Return the weight of the edge between the given items.
-
-        Return 0 if item1 and item2 are not adjacent.
-
-        Preconditions:
-            - item1 and item2 are vertices in this graph
-        """
-        v1 = self._vertices[item1]
-        v2 = self._vertices[item2]
-        return v1.neighbours.get(v2, 0)
-
-    def average_weight(self, item: Any) -> float:
-        """Return the average weight of the edges adjacent to the vertex corresponding to item.
-
-        Raise ValueError if item does not corresponding to a vertex in the graph.
-        """
-        if item in self._vertices:
-            v = self._vertices[item]
-            return sum(v.neighbours.values()) / len(v.neighbours)
-        else:
-            raise ValueError
-
-    def to_networkx(self, max_vertices: int = 5000) -> nx.Graph:
-        """Convert this graph into a networkx Graph.
-
-        max_vertices specifies the maximum number of vertices that can appear in the graph.
-        (This is necessary to limit the visualization output for large graphs.)
-
-        Note that this method is provided for you, and you shouldn't change it.
-        """
-        graph_nx = nx.Graph()
-        for v in self._vertices.values():
-            graph_nx.add_node(v.item, kind=v.kind)
-
-            for u in v.neighbours.keys():
-                if graph_nx.number_of_nodes() < max_vertices:
-                    graph_nx.add_node(u.item, kind=u.kind)
-
-                if u.item in graph_nx.nodes:
-                    graph_nx.add_edge(v.item, u.item, weight=v.neighbours[u])
+                if u.name in graph_nx.nodes:
+                    graph_nx.add_edge(v.name, u.name)
 
             if graph_nx.number_of_nodes() >= max_vertices:
                 break
 
         return graph_nx
 
-    ############################################################################
-    # Part 2, Q2b
-    ############################################################################
-    def get_similarity_score(self, item1: Any, item2: Any,
-                             score_type: str = 'unweighted') -> float:
-        """Return the similarity score between the two given items in this graph.
+    def load_graph(self, rest_file: str) -> Graph:
+        """Return a restaurant graph corresponding to the given datasets.
 
-        score_type is one of 'unweighted' or 'strict', corresponding to the
-        different ways of calculating weighted graph vertex similarity, as described
-        on the assignment handout.
-
-        Raise a ValueError if item1 or item2 do not appear as vertices in this graph.
-
-        Preconditions:
-            - score_type in {'unweighted', 'strict'}
+        The CSV file should have the columns 'Category', 'Restaurant Address', 'Name',
+        'Restaurant Price Range', 'Restaurant Latitude', 'Restaurant Longitude'
+        and 'Review Rates'.
         """
+        graph = Graph()
 
+        with open(rest_file, 'r') as file:
+            next(file)
+            reader = csv.reader(file)
+            for row in reader:
+                category, address, name, price_range, latitude, longitude, review_rate = row
+                price_min, price_max = map(int, price_range.replace('$', '').split('-'))
+                latitude = float(latitude)
+                longitude = float(longitude)
+                review_rate = float(review_rate)
 
-    ############################################################################
-    # Part 2, Q2c
-    ############################################################################
-    def recommend_books(self, book: str, limit: int,
-                        score_type: str = 'unweighted') -> list[str]:
-        """Return a list of up to <limit> recommended books based on similarity to the given book.
+                graph.add_vertex(category, address, name, price_range, latitude, longitude, review_rate)
 
-        score_type is one of 'unweighted' or 'strict', corresponding to the
-        different ways of calculating weighted graph vertex similarity, as described
-        on the assignment handout. The corresponding similarity score formula is used
-        in this method (whenever the phrase "similarity score" appears below).
-
-        The return value is a list of the titles of recommended books, sorted in
-        *descending order* of similarity score. Ties are broken in descending order
-        of book title. That is, if v1 and v2 have the same similarity score, then
-        v1 comes before v2 if and only if v1.item > v2.item.
-
-        The returned list should NOT contain:
-            - the input book itself
-            - any book with a similarity score of 0 to the input book
-            - any duplicates
-            - any vertices that represents a user (instead of a book)
-
-        Up to <limit> books are returned, starting with the book with the highest similarity score,
-        then the second-highest similarity score, etc. Fewer than <limit> books are returned if
-        and only if there aren't enough books that meet the above criteria.
-
-        Preconditions:
-            - book in self._vertices
-            - self._vertices[book].kind == 'book'
-            - limit >= 1
-            - score_type in {'unweighted', 'strict'}
-        """
-
-
-if __name__ == '__main__':
-    import doctest
-
-    doctest.testmod(verbose=True)
-
-    # When you are ready to check your work with python_ta, uncomment the following lines.
-    # (In PyCharm, select the lines below and press Ctrl/Cmd + / to toggle comments.)
-    # You can use "Run file in Python Console" to run both pytest and PythonTA,
-    # and then also test your methods manually in the console.
-    # import python_ta
-    # python_ta.check_all(config={
-    #     'max-line-length': 120
-    # })
+        return graph
