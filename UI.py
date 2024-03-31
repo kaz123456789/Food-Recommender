bdb2c7a8f3454060b45d0ceed9439b13721ae9ed
KimberlyFU <kimberlyfu006@gmail.com>
2024-03-31 01:31:51 -0400

    Update UI.py

--- UI.py
+++ UI.py
@@ -9,3 +9,48 @@ This module contains the mainfunction of our UI.
 
 This file is Copyright (c) Kathleen Wang, Jiner Zhang, Kimberly Fu, and Yanting Fan.
 """
+# from prompt_toolkit import prompt
+
+import tkinter as tk
+from tkinter import simpledialog, messagebox
+
+
+def save_input():
+    """
+    save user input in 'user_input.txt'
+    """
+
+    with open("user_input.txt", "w") as file:
+        for i in range(1, 4):
+            file.write(get_user_input(i))
+
+
+def get_user_input(n: int) -> str:
+    """
+    get user inputs
+    """
+    while True:
+        user_input = simpledialog.askstring("Input", f"Enter your input{n}:") + '\n'
+        if user_input == '\n':
+            messagebox.showwarning("Warning", "Input cannot be empty. Please enter your input.")
+        else:
+            messagebox.showinfo("Success", f"Input{n} saved successfully!")
+            break
+    return user_input
+
+
+def main():
+    """
+    main function
+    """
+    root = tk.Tk()
+    root.title("Please input your preference!")
+
+    button = tk.Button(root, text="Enter Input", command=save_input)
+    button.pack(pady=20)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
