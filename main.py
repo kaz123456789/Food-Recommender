6ed25d133667116deb7004f168209581b02fac9d
fries anyone? <ffanyanting@gmail.com>
2024-03-31 16:13:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -35,24 +35,37 @@ if __name__ == "__main__":
 
     all_users = AllUsers()
     while not quit_game:
-        # The first round of recommending restaurants (randomly).
         user_name = read_latest_input_from_csv(read_lines, 'data/user_inputs.csv')
         user = User(user_name)
+
         if user not in all_users.list_of_users:
             all_users.list_of_users.append(user)
             print(f"Welcome to FOODER, {user_name}!")
         else:
             print(f"Welcome back to FOODER, {user_name}! We are confident to find you a "
                   f"matching restaurant this time, too!")
-        random_res = user.recommend_restaurants(restaurant_graph)
-        for res in random_res:
-            print(res.name)
-        satisfy = read_latest_input_from_csv(read_lines, 'data/user_inputs.csv')
-        user.last_visit(restaurant_graph.vertices()[satisfy])
-        user.feedback_on_last_visit(satisfy in restaurant_graph.vertices().keys())
 
-        # Ask user if they want to try again, if no, quit the game, is yes, continue.
-        if_quit = read_latest_input_from_csv(read_lines, 'data/user_inputs.csv')
+        if not user.last_visited_restaurant:
+            you_may_like = restaurant_graph.most_similar_restaurants(user.last_visited_restaurant.name)
+            restaurant_graph.most_similar_restaurants_all_connected(user.last_visited_restaurant.name)
+        else:
+            random_rest = restaurant_graph.get_random_restaurant()
+            satisfy_with_first = read_latest_input_from_csv(read_lines, 'data/user_inputs.csv')
+
+            if satisfy_with_first.lower() == 'yes':
+                quit_game = True
+                break
+            else:
+                random_rests = user.recommend_restaurants(restaurant_graph)
+                for res in random_rests:
+                    print(res.name)
+                satisfy = read_latest_input_from_csv(read_lines, 'data/user_inputs.csv')
+                user.last_visit(restaurant_graph.vertices()[satisfy])
+                user.feedback_on_last_visit(satisfy in restaurant_graph.vertices().keys())
+
+            # Ask user if they want to try again, if no, quit the game, is yes, continue.
+            if_quit = read_latest_input_from_csv(read_lines, 'data/user_inputs.csv')
+
         if if_quit == 'yes':
             quit_game = True
 
